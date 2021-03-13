98c654bf993847216dbd6404239e9d70d2767988
gil-son <gilszon@gmail.com>
2021-03-13 19:53:53 -0300

    Scripts SQL

--- file.sql
+++ file.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS financial_budget;
 USE financial_budget;
 
-CREATE TABLE IF NOT EXISTS input(
-id_input INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS budget(
+id_budget INT PRIMARY KEY AUTO_INCREMENT,
 description VARCHAR(50) NOT NULL,
 category  VARCHAR(50) NOT NULL,
 value DOUBLE(8,2) NOT NULL,
@@ -11,15 +11,8 @@ date datetime DEFAULT CURRENT_TIMESTAMP
 );
 
 
-CREATE TABLE IF NOT EXISTS output(
-id_output INT PRIMARY KEY AUTO_INCREMENT,
-description VARCHAR(50) NOT NULL,
-category  VARCHAR(50) NOT NULL,
-value DOUBLE(8,2) NOT NULL,
-status ENUM("To do", "Doing", "Done"),
-date datetime DEFAULT CURRENT_TIMESTAMP
-);
+-- INSERT INTO budget(description, category, value, status) VALUES('Description','Category',4000,'To do');
+-- SELECT * FROM budget;
 
--- INSERT INTO input(description, category, value, status) VALUES('Description','Category',4000,'To do');
--- SELECT * FROM input;
+
 
