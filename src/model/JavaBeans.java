7f9f29afa4abcfb8a2d448b2fe762f12d818d548
gil-son <gilszon@gmail.com>
2021-03-14 19:39:37 -0300

    Java Beans

--- src/model/JavaBeans.java
+++ src/model/JavaBeans.java
@@ -3,12 +3,12 @@ package model;
 import java.util.Date;
 
 public class JavaBeans {
-	private int id_budget;
+	private String id_budget;
 	private String description;
 	private String category;
 	private String value;
 	private String status;
-	private Date date;
+	private String date;
 	
 	
 	
@@ -17,8 +17,11 @@ public class JavaBeans {
 		// TODO Auto-generated constructor stub
 	}
 
+	
+	
 
-	public JavaBeans(int id_budget, String description, String category, String value, String status, Date date) {
+
+	public JavaBeans(String id_budget, String description, String category, String value, String status, String date) {
 		super();
 		this.id_budget = id_budget;
 		this.description = description;
@@ -27,44 +30,83 @@ public class JavaBeans {
 		this.status = status;
 		this.date = date;
 	}
-	
-	
-	public int getId_budget() {
+
+
+
+
+
+	public String getId_budget() {
 		return id_budget;
 	}
-	public void setId_budget(int id_budget) {
+
+
+
+	public void setId_budget(String id_budget) {
 		this.id_budget = id_budget;
 	}
+
+
+
 	public String getDescription() {
 		return description;
 	}
+
+
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+
+
 	public String getCategory() {
 		return category;
 	}
+
+
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
+
+
 	public String getValue() {
 		return value;
 	}
+
+
+
 	public void setValue(String value) {
 		this.value = value;
 	}
+
+
+
 	public String getStatus() {
 		return status;
 	}
+
+
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	public Date getDate() {
+
+
+
+	public String getDate() {
 		return date;
 	}
-	public void setDate(Date date) {
+
+
+
+	public void setDate(String date) {
 		this.date = date;
 	}
+
+
+	
 	
 	
 	
